6cd9bec3bf28629aaf6e5c456d21bd100c660ddf
danibene <34680344+danibene@users.noreply.github.com>
2023-09-30 11:14:58 -0400

    add sample timestamp conversion

--- src/tempbeat/preprocessing/preprocessing_utils.py
+++ src/tempbeat/preprocessing/preprocessing_utils.py
@@ -1,4 +1,5 @@
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
+from warnings import warn
 
 import numpy as np
 from neurokit2.hrv.intervals_utils import _intervals_successive
@@ -84,3 +85,132 @@ def rri_to_peak_time(rri: np.ndarray, rri_time: np.ndarray) -> np.ndarray:
     peak_time = np.sort(np.concatenate((rri_time, times_to_insert)))
 
     return peak_time
+
+
+def samp_to_timestamp(
+    samp: Union[int, np.ndarray],
+    sampling_rate: int = 1000,
+    sig_time: Optional[np.ndarray] = None,
+) -> Union[float, np.ndarray]:
+    """
+    Convert sample indices to timestamps.
+
+    This function takes sample indices and converts them to timestamps based on the provided
+    sampling rate and optional signal time array. If a signal time array is provided, the
+    function will ensure that the returned timestamps do not exceed the last timestamp in
+    the array.
+
+    Parameters
+    ----------
+    samp : Union[int, np.ndarray]
+        Sample index or array of sample indices.
+    sampling_rate : int, optional
+        The sampling rate of the signal, in Hz. Default is 1000 Hz.
+    sig_time : Optional[np.ndarray], optional
+        Array of timestamps corresponding to each sample index. If not provided, timestamps
+        are calculated based on the sampling rate.
+
+    Returns
+    -------
+    Union[float, np.ndarray]
+        Timestamp or array of timestamps corresponding to the input sample index or array.
+
+    Warnings
+    --------
+    - If a sample index is less than 0, it is changed to 0.
+    - If a sample index is greater than the last index, it is changed to the last index.
+    """
+    less_than_zero = np.where(samp < 0)
+    if np.any(less_than_zero):
+        warn(
+            "Warning: the sample index is less than 0. Changing the sample index to 0."
+        )
+        samp[less_than_zero] = 0
+
+    if sig_time is None:
+        timestamp = samp / sampling_rate - 1 / sampling_rate
+    else:
+        bigger_than_last_index = np.where(samp >= len(sig_time))
+        if np.any(bigger_than_last_index):
+            warn(
+                """Warning: the sample index is greater than the last index.
+                Changing the sample index to the last index."""
+            )
+            samp[bigger_than_last_index] = len(sig_time) - 1
+        timestamp = sig_time[samp]
+
+    return timestamp
+
+
+def timestamp_to_samp(
+    timestamp: Union[float, np.ndarray],
+    sampling_rate: int = 1000,
+    sig_time: Optional[np.ndarray] = None,
+    check_greater_than_last: bool = True,
+) -> np.ndarray:
+    """
+    Convert timestamps to sample indices.
+
+    This function takes timestamps and converts them to sample indices based on the provided
+    sampling rate and optional signal time array.
+
+    Parameters
+    ----------
+    timestamp : Union[float, np.ndarray]
+        Timestamp or array of timestamps.
+    sampling_rate : int, optional
+        The sampling rate of the signal, in Hz. Default is 1000 Hz.
+    sig_time : Optional[np.ndarray], optional
+        Array of timestamps corresponding to each sample index. If not provided, timestamps
+        are calculated based on the sampling rate.
+    check_greater_than_last : bool, optional
+        Whether to check if the calculated sample index is greater than the last index in
+        `sig_time`. Default is True.
+
+    Returns
+    -------
+    np.ndarray
+        Array of sample indices corresponding to the input timestamp or array.
+
+    Warnings
+    --------
+    - If a sample index is greater than the last index, it is changed to the last index.
+    - If a sample index is less than 0, it is changed to 0.
+    """
+    timestamp = np.array(timestamp)
+    if timestamp.size == 1:
+        timestamp = np.array([timestamp])
+
+    if sig_time is None:
+        sig_time = [0]
+        if check_greater_than_last:
+            warn(
+                """Warning: to check whether the sample is greater than the last sample index,
+                 sig_time must be given."""
+            )
+            check_greater_than_last = False
+        samp = np.array(
+            np.round((timestamp - sig_time[0] + 1 / sampling_rate) * sampling_rate)
+        ).astype(int)
+    else:
+        samp = np.array([np.argmin(np.abs(sig_time - t)) for t in timestamp]).astype(
+            int
+        )
+
+    if check_greater_than_last:
+        greater_than_len = np.where(samp > len(sig_time) - 1)
+        if np.any(greater_than_len):
+            warn(
+                """Warning: the sample index is greater than the last sample index.
+                 Changing the sample index to the last sample index."""
+            )
+            samp[greater_than_len] = len(sig_time) - 1
+
+    less_than_zero = np.where(samp < 0)
+    if np.any(less_than_zero):
+        warn(
+            "Warning: the sample index is less than 0. Changing the sample index to 0."
+        )
+        samp[less_than_zero] = 0
+
+    return samp
--- tests/preprocessing/test_preprocessing_utils.py
+++ tests/preprocessing/test_preprocessing_utils.py
@@ -3,6 +3,8 @@ import numpy as np
 from tempbeat.preprocessing.preprocessing_utils import (
     peak_time_to_rri,
     rri_to_peak_time,
+    samp_to_timestamp,
+    timestamp_to_samp,
 )
 
 
@@ -106,4 +108,89 @@ class TestRRIToPeakTime:
         peak_time = rri_to_peak_time(rri, rri_time)
         result = peak_time_to_rri(peak_time)
         expected = (np.array([750, 500, 1000]), np.array([1.75, 2.25, 3.25]))
+        np.testing.assert_array_equal(result, expected)
+
+
+class TestSampToTimestamp:
+    """
+    Test cases for the samp_to_timestamp function.
+    """
+
+    @staticmethod
+    def test_samp_to_timestamp_basic() -> None:
+        """
+        Test samp_to_timestamp with a basic example.
+
+        The function should correctly convert sample indices to timestamps.
+        """
+        sampling_rate = 1000
+        samp = np.array([100, 500, 1000])
+        result = samp_to_timestamp(samp, sampling_rate=sampling_rate)
+        # Subtract 1/sampling_rate to account for the fact that the first sample is at time 0.
+        expected = np.array([0.1, 0.5, 1.0]) - 1 / sampling_rate
+        np.testing.assert_array_equal(result, expected)
+
+    @staticmethod
+    def test_samp_to_timestamp_with_sig_time() -> None:
+        """
+        Test samp_to_timestamp with providing sig_time.
+
+        The function should correctly convert sample indices to timestamps with sig_time.
+        """
+        sig_time = np.array([0.0, 0.1, 0.2, 0.3, 0.4])
+        samp = np.array([0, 2, 4])
+        result = samp_to_timestamp(samp, sig_time=sig_time)
+        expected = np.array([0.0, 0.2, 0.4])
+        np.testing.assert_array_equal(result, expected)
+
+    @staticmethod
+    def test_samp_to_timestamp_with_index_higher_than_sig_time_len() -> None:
+        """
+        Test samp_to_timestamp with providing sig_time and sample index higher than sig_time length.
+
+        The function should take the last timestamp in sig_time as the timestamp for the sample index
+        that is higher than sig_time length.
+        """
+        sig_time = np.array([0.0, 0.1, 0.2, 0.3, 0.4])
+        samp = np.array([0, 2, 4, 6])
+        result = samp_to_timestamp(samp, sig_time=sig_time)
+        expected = np.array([0.0, 0.2, 0.4, 0.4])
+        np.testing.assert_array_equal(result, expected)
+
+
+class TestTimestampToSamp:
+    """
+    Test cases for the timestamp_to_samp function.
+    """
+
+    @staticmethod
+    def test_timestamp_to_samp_basic() -> None:
+        """
+        Test timestamp_to_samp with a basic example.
+
+        The function should correctly convert timestamps to sample indices.
+        """
+        sampling_rate = 1000
+        timestamp = np.array([0.1, 0.5, 1.0])
+        result = timestamp_to_samp(timestamp, sampling_rate=sampling_rate)
+        expected = np.array([101, 501, 1001])
+        np.testing.assert_array_equal(result, expected)
+
+        sampling_rate = 100
+        timestamp = np.array([0.1, 0.5, 1.0])
+        result = timestamp_to_samp(timestamp, sampling_rate=sampling_rate)
+        expected = np.array([11, 51, 101])
+        np.testing.assert_array_equal(result, expected)
+
+    @staticmethod
+    def test_timestamp_to_samp_with_sig_time() -> None:
+        """
+        Test timestamp_to_samp with providing sig_time.
+
+        The function should correctly convert timestamps to sample indices with sig_time.
+        """
+        sig_time = np.array([0.0, 0.1, 0.2, 0.3, 0.4])
+        timestamp = np.array([0.0, 0.2, 0.4])
+        result = timestamp_to_samp(timestamp, sig_time=sig_time)
+        expected = np.array([0, 2, 4])
         np.testing.assert_array_equal(result, expected)

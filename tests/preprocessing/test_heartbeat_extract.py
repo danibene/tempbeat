ed343f208bc20994f879db71646b2a017e703bae
danibene <34680344+danibene@users.noreply.github.com>
2023-10-01 21:27:03 -0400

    use example instead of simulated ecg

--- tests/preprocessing/test_heartbeat_extract.py
+++ tests/preprocessing/test_heartbeat_extract.py
@@ -1,5 +1,5 @@
+import neurokit2 as nk
 import numpy as np
-from neurokit2 import ecg_simulate
 
 from tempbeat.preprocessing.heartbeat_extract import temp_hb_extract
 from tempbeat.preprocessing.preprocessing_utils import sampling_rate_to_sig_time
@@ -13,45 +13,40 @@ class TestTempHbExtract:
     @staticmethod
     def test_temp_hb_extract_regression() -> None:
         """
-        Test temp_hb_extract with simulated ECG data.
+        Test temp_hb_extract with example ECG data.
 
         The function should extract the peak times such that they are within 0.01 seconds of the expected values.
-        The expected values were obtained by running the function on the simulated ECG data with the same parameters.
+        The expected values were obtained by running the function on the example ECG data with the same parameters.
         """
-        sampling_rate = 1000
-        random_state = 42
-        random_state_distort = 43
-        sig = ecg_simulate(
-            duration=20,
-            sampling_rate=sampling_rate,
-            random_state=random_state,
-            random_state_distort=random_state_distort,
-        )
+        sampling_rate = 100
+        data = nk.data("bio_resting_5min_100hz")
+        sig = data["ECG"]
         sig_time = sampling_rate_to_sig_time(sig, sampling_rate)
         peaks = temp_hb_extract(sig, sig_time=sig_time, sampling_rate=sampling_rate)
         np.testing.assert_allclose(
-            peaks,
+            peaks[:20],
             np.array(
                 [
-                    0.85,
-                    1.72,
-                    2.59,
-                    3.44,
-                    4.3,
-                    5.18,
-                    6.31,
-                    7.45,
-                    8.58,
-                    9.72,
-                    10.85,
-                    11.99,
-                    12.87,
-                    13.75,
-                    14.81,
-                    15.88,
-                    16.95,
-                    18.02,
-                    18.87,
+                    2.85,
+                    3.64,
+                    4.43,
+                    5.23,
+                    6.02,
+                    6.82,
+                    7.48,
+                    8.12,
+                    8.78,
+                    9.45,
+                    10.1,
+                    10.74,
+                    11.39,
+                    12.07,
+                    12.76,
+                    13.72,
+                    14.69,
+                    15.38,
+                    16.09,
+                    16.75,
                 ]
             ),
             rtol=0.01,

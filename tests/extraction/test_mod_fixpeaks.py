6799263a323ad8fbb27338bf4e828a36321cbbd2
danibene <34680344+danibene@users.noreply.github.com>
2024-03-19 08:27:45 -0400

    address type errors in test_mod_fixpeaks

--- tests/extraction/test_mod_fixpeaks.py
+++ tests/extraction/test_mod_fixpeaks.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import neurokit2 as nk
 import numpy as np
 import numpy.random
@@ -7,7 +9,7 @@ from tempbeat.extraction.mod_fixpeaks import signal_fixpeaks
 
 
 @pytest.fixture
-def testpeaks_for_neurokit_method():
+def testpeaks_for_neurokit_method() -> np.ndarray:
     signal = nk.signal_simulate(duration=20, sampling_rate=1000, frequency=1)
     peaks_true = nk.signal_findpeaks(signal)["Peaks"]
     peaks = np.delete(peaks_true, [5, 6, 7, 8, 9, 10, 15, 16, 17, 19])  # create gaps
@@ -18,8 +20,8 @@ def testpeaks_for_neurokit_method():
 
 @pytest.mark.parametrize("interval_max", [None, 1.5, 2.0])
 def test_neurokit_method_returns_only_positive_indices(
-    testpeaks_for_neurokit_method, interval_max
-):
+    testpeaks_for_neurokit_method: np.ndarray, interval_max: Optional[float]
+) -> None:
     peaks_corrected = signal_fixpeaks(
         peaks=testpeaks_for_neurokit_method,
         interval_min=0.5,
@@ -31,8 +33,8 @@ def test_neurokit_method_returns_only_positive_indices(
 
 @pytest.mark.parametrize("interval_max", [None, 1.5, 2.0])
 def test_neurokit_method_returns_no_duplicates(
-    testpeaks_for_neurokit_method, interval_max
-):
+    testpeaks_for_neurokit_method: np.ndarray, interval_max: Optional[float]
+) -> None:
     peaks_corrected = signal_fixpeaks(
         peaks=testpeaks_for_neurokit_method,
         interval_min=0.5,
@@ -44,8 +46,8 @@ def test_neurokit_method_returns_no_duplicates(
 
 @pytest.mark.parametrize("interval_max", [None, 1.5, 2.0])
 def test_neurokit_method_returns_strictly_increasing_indices(
-    testpeaks_for_neurokit_method, interval_max
-):
+    testpeaks_for_neurokit_method: np.ndarray, interval_max: Optional[float]
+) -> None:
     peaks_corrected = signal_fixpeaks(
         peaks=testpeaks_for_neurokit_method,
         interval_min=0.5,

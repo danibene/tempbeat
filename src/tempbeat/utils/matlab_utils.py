fb3690ddc823dff76846d9e112ba530ea64c4155
danibene <34680344+danibene@users.noreply.github.com>
2024-03-20 00:13:12 -0400

    update precommit and address type errors in matlab_utils

--- src/tempbeat/utils/matlab_utils.py
+++ src/tempbeat/utils/matlab_utils.py
@@ -1,22 +1,27 @@
-# Utils to get matlab engine
-from typing import Any
+from typing import Any, Optional
+
+global_eng: Optional[Any] = None  # Use Any for the type hint
 
 
 def set_matlab() -> None:
+    global global_eng
     try:
         import matlab.engine
+
+        global_eng = matlab.engine.start_matlab()
     except ImportError:
         raise ImportError("Matlab engine is not installed.")
-    global global_eng  # type: ignore
-    global_eng = matlab.engine.start_matlab()
 
 
 def get_matlab() -> Any:
-    global global_eng  # type: ignore
-    eng = global_eng
-    return eng
+    global global_eng
+    if global_eng is None:
+        raise Exception("Matlab engine is not initialized. Call set_matlab() first.")
+    return global_eng
 
 
 def quit_matlab() -> None:
     global global_eng
-    global_eng.quit()
+    if global_eng is not None:
+        global_eng.quit()
+        global_eng = None  # Properly handle reinitialization cases
